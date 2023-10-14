3267d369068d4e5ac1f7d930dadd0ffc26930cf6
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Pro.local>
2023-10-13 21:47:11 -0700

    added routes, now it should actually work (still theoretical)

--- solid_phoenix/lib/solid_phoenix_web.ex
+++ solid_phoenix/lib/solid_phoenix_web.ex
@@ -49,6 +49,20 @@ defmodule SolidPhoenixWeb do
     end
   end
 
+  def view do
+    quote do
+      use Phoenix.View,
+        root: "lib/dreamshare_web/templates",
+        namespace: SolidPhoenixWeb
+
+      # Import convenience functions from controllers
+      import Phoenix.Controller,
+        only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
+
+      # Include shared imports and aliases for views
+    end
+  end
+
   def live_view do
     quote do
       use Phoenix.LiveView,
